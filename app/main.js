f98744e6347d39a469d06bc94c4046ea60c131e2
Aditya C <drumsapkota@gmail.com>
2017-07-27 16:49:40 +0530

    Working youtube downloader with Ajaz

--- app/main.js
+++ app/main.js
@@ -4,6 +4,6 @@ var Menu = electron.Menu;
 var app = electron.app;
 
 app.on('ready',function(){
-	var appWindow = new BrowserWindow();
+	var appWindow = new BrowserWindow({width:600, height:400, autoHideMenuBar:true,title:"DSFtube"});
 	appWindow.loadURL('file://'+__dirname+'/index.html');
 });
--- process/js/render.js
+++ process/js/render.js
@@ -2,25 +2,45 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
-
 var fs = eRequire('fs');
-var loadData = JSON.parse(fs.readFileSync(dataLocation));
+var ytdl = eRequire('ytdl-core');
+//var loadData = JSON.parse(fs.readFileSync(dataLocation));
 
 
 class HelloMessage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {mydata: loadData};
+    this.handleChange = this.handleChange.bind(this);
+  	this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {videoId:''};
   }
+//  componentDidUpdate:function(){
+//  	fs.writeFile(dataLocation,data,format,function(err){
+//  		if(err){
+//  			console.log(err);
+//  		}
+//  	})
+//  }
+  handleSubmit(i){
+  	i.preventDefault();
+  	//this.setState({videoId:e.target.value});
+  	console.log("Submitted "+this.state.videoId);
 
-  
+  	ytdl(this.state.videoId).pipe(fs.createWriteStream('video.flv'));
+  }
+  handleChange(i){
+  	this.setState({videoId:i.target.value});
+  	console.log("Typing....");
+  }
   render() {
   	var alldata = this.state.mydata;
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <span>{alldata[1].STU_ID}</span>
-      </div>
+    return (<div><form>
+		      <div className="form-group">
+		        
+		        <input className="form-control" placeholder="Video URL" onChange={this.handleChange} value={this.state.videoId}/>
+		        <button className="btn btn-primary" onClick={this.handleSubmit}>Download</button>
+		      </div>
+      </form></div>
     );
   }
 }
